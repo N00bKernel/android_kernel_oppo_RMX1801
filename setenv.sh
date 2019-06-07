406092881d098975c12687ad68b2f3c8c5b20204
chankruze <chankruze@gmail.com>
2019-06-08 01:40:39 +0530

    RMX1801: N00b Kernel Initial Commit
    Signed-off-by: chankruze <chankruze@gmail.com>

--- setenv.sh
+++
@@ -1,8 +0,0 @@
-export PATH=/usr/lib/ccache:~/toolchains/gcc/aarch64-linux-android-4.9/bin:$PATH
-export CROSS_COMPILE=aarch64-linux-android-
-export ARCH=arm64 
-export SUBARCH=arm64
-export OPPO_TARGET_DEVICE=18321
-export KBUILD_BUILD_HOST=Ubuntu-18.04-bionic
-export KBUILD_BUILD_USER=redfeast😎
-export KERNELSRC=~/50G/linux
